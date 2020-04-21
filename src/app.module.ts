d0ea82001ba69dc0de097d5935f7582046049cd0
maximosdrr <46094587+maximosdrr@users.noreply.github.com>
2020-04-21 18:16:30 -0300

    src/modules/user/user.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['src/config/.api.secret.env'],
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot(),
-    UserModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['src/config/.api.secret.env'],
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRoot(),
+        UserModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
