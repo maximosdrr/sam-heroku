52586c7525c5847b9da790e72c97f6ad89ff4851
maximosdrr <maximosdrr@gmail.com>
2020-04-22 19:00:10 -0300

    test(modules/auth/tests): tests for routes login
    Tests if user login is valid or invalid implemented

--- src/modules/auth/tests/app-e2e-spec.ts
+++ src/modules/auth/tests/app-e2e-spec.ts
@@ -1,13 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { AppModule } from '../../../app.module';
+import { INestApplication } from '@nestjs/common';
+import { LoginInterface } from '../../user/interfaces/login.interface';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../user/entitys/user.entity';
+import { Repository } from 'typeorm/repository/Repository';
+import { UserService } from '../../user/user.service';
 
 describe('AppController (e2e)', () => {
   const app = 'http://localhost:3000';
+  let repository: Repository<User>;
 
-  it('/ (GET)', () => {
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        { provide: getRepositoryToken(User), useClass: Repository },
+      ],
+    }).compile();
+
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it('Check a valid user login', () => {
+    const loginData: LoginInterface = {
+      username: 'maximosdrr',
+      password: '123456',
+    };
     return request(app)
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send(loginData)
+      .expect(({ body }) => {
+        expect(body.accessToken).toBeDefined();
+      });
+  });
+
+  it('Check invalid user login', () => {
+    const loginData: LoginInterface = {
+      username: 'any',
+      password: 'any',
+    };
+
+    return request(app)
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send(loginData)
+      .expect(({ body }) => {
+        expect(body.accessToken).toBeUndefined();
+        expect(401);
+        console.log(body.accessToken);
+      });
   });
 });
